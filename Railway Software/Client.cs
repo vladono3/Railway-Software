dcce05cac3516a9e8a61bed128e2976d4f9611e5
Lenovo <Lenovo@DESKTOP-ISLSHM0>
2025-03-04 11:09:55 -0800

    Add project files.

---
+++ Railway Software/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Railway_Software
+{
+    class Client
+    {
+        int id;
+        string name;
+        string last_name;
+        int age;
+
+        public void PrintClient()
+        {
+            Console.WriteLine($"Client: {name} {last_name} age: {age}");
+        }
+
+        public Client()
+        {
+            name = last_name = string.Empty;
+            age = 0;
+        }
+
+        public Client(string _name, string _last_name, int _age)
+        {
+            name = _name;
+            last_name = _last_name;
+            age = _age;
+        }
+    }
+}
---
+++ Railway Software/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Railway_Software.Client;
+using static Railway_Software.Employee;
+namespace Railway_Software
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client c1 = new Client("Vlad", "Onofrei", 20);
+            c1.PrintClient();
+
+            Employee e1 = new Employee("Ruth", "Candi", 20);
+            e1.PrintClient();
+        }
+    }
+}
